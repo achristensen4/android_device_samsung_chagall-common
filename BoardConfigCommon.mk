944615bcb2bec0da51fe539c65a30d794e706df0
sspencer10 <seanhoyt963@gmail.com>
2015-12-01 01:22:29 -0600

    Initial common

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from common exynos5420-common
+-include device/samsung/exynos5420-common/BoardConfigCommon.mk
+
+# Camera
+# COMMON_GLOBAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+BOARD_NEEDS_MEMORYHEAPION := true
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
+
+# Charging mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_BATTERY_DEVICE_NAME := battery
+
+# FIMG2D
+BOARD_USES_FIMGAPI_V4L2 := false
+
+# Graphics
+OVERRIDE_RS_DRIVER := libRSDriverArm.so
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := chagall
+
+# SurfaceFlinger
+BOARD_USES_SYNC_MODE_FOR_MEDIA := true
+
+# Wifi
+BOARD_HAVE_SAMSUNG_WIFI          := true
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+
+# custom additions to updater-script
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagall-common/releasetools/ota_from_target_files
---
+++ system_prop.mk
@@ -0,0 +1,8 @@
+#
+# Common system properties for chagall-common
+#
+
+# Randomly from stock
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.ril.gprsclass=10 \
+ro.ril.hsxpa=1
