d0b9b25834d85cd84b4ffdfc050afd9c9016a562
Sean Hoyt <seanhoyt963@gmail.com>
2015-12-03 00:23:08 -0600

    Remove duplicated battery name
    This is in exynos5420-common

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,6 @@ TARGET_KERNEL_SOURCE := kernel/samsung/klimtwifi
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := battery
 
 # FIMG2D
 BOARD_USES_FIMGAPI_V4L2 := false
